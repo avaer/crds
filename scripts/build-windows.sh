294d0116f61e3f8d8825600d288fdb63752c23fc
Adrian Biedrzycki <a@modules.io>
2017-08-03 15:37:42 -0700

    Add windows build script

---
+++ scripts/build-windows.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VERSION="8.1.3";
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "$DIR"/..;
+
+rm -Rf node_modules installed data/mod-hashes.json 'Zeo VR.lnk' node "node-v$VERSION-win-x64" windows-release.zip
+npm install
+curl "https://nodejs.org/dist/v$VERSION/node-v$VERSION-win-x64.zip" >node.zip
+unzip node.zip
+rm node.zip
+mv "node-v$VERSION-win-x64" node
+cp node/node_modules/npm/bin/npm.cmd node/
+cp 'scripts/lib/windows/CRDS.lnk' .
+zip -r windows-release.zip . -x /.git* -x /data*
+rm -R node
+rm 'CRDS.lnk'
+
+popd;
